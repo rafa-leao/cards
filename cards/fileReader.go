c10039d0a6db056e813540ea7b7c845800ee454d
rafa-leao <rleao53@gmail.com>
2019-06-06 19:30:35 -0300

    Tinny change on logging erros about open CSV files

--- cards/fileReader.go
+++ cards/fileReader.go
@@ -11,13 +11,13 @@ func ReadCSV(fileName string) [][]string {
 	csvFile, err := os.Open(fileName)
 
 	if err != nil {
-		log.Fatalln("Something went wrong when trying to open your file", err)
+		log.Fatalln(err)
 	}
 
 	result, err := csv.NewReader(csvFile).ReadAll()
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 
 	return result
