800760d2103d19fc9d8b306948ccb88d730bddb1
rafa-leao <rleao53@gmail.com>
2019-06-18 11:51:06 -0300

    Shuffle cards when starts practicing

--- cards/playCards.go
+++ cards/playCards.go
@@ -1,29 +1,80 @@
 package cards
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
 
-func Practice(cardsToPractice [][]string) {
+type deckFace struct {
+	front string
+	back  string
+}
 
-	type deckFace struct {
-		front string
-		back  string
-	}
+func createCards(cardsToPractice [][]string) (cards []deckFace) {
 
-	for _, card:= range cardsToPractice {
+	cards = make([]deckFace, len(cardsToPractice))
 
-		card := deckFace {
+	for i, card := range cardsToPractice {
+
+		cards[i] = deckFace{
 			front: card[0],
 			back:  card[1],
 		}
+	}
 
-		fmt.Printf("%s = ", card.front)
+	return
+}
 
-		var answer string
-		fmt.Scanf("%s", &answer)
+func randomNumber(number int) int {
 
-		if answer != card.back{
-			fmt.Println("Wrong answer. Do not give up! Try again!")
-			break
+	return rand.New(rand.NewSource(time.Now().UnixNano())).Intn(number)
+
+}
+
+func cardInCardsAnswered(card deckFace, cardsAnswered []deckFace) bool {
+
+	for _, cardAnswered := range cardsAnswered {
+
+		if card == cardAnswered {
+			return true
 		}
 	}
+
+	return false
+
+}
+
+func Practice(cardsToPractice [][]string) {
+
+	cards := createCards(cardsToPractice)
+
+	var (
+		card            deckFace
+		cardsAnswered []deckFace
+	)
+
+	for totalOfCards := len(cards); totalOfCards > len(cardsAnswered); {
+
+		card = cards[randomNumber(len(cardsToPractice))]
+
+		if !cardInCardsAnswered(card, cardsAnswered) {
+
+			fmt.Printf("%s = ", card.front)
+
+			var answer string
+			fmt.Scan(&answer)
+
+			if answer != card.back {
+				fmt.Println("Wrong answer. Do not give up! Try again!")
+				os.Exit(1)
+			} else {
+				cardsAnswered = append(cardsAnswered, card)
+			}
+		}
+	}
+
+	fmt.Println("You got every thing!")
+	os.Exit(1)
 }
