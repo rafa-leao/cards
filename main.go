7ff370a597d813f6795794532df7d026597c871e
rafa-leao <rleao53@gmail.com>
2019-06-19 19:41:05 -0300

    Add learning feature

--- main.go
+++ main.go
@@ -8,15 +8,21 @@ import (
 )
 
 var (
-	playCSV = flag.String("play", "", "Path for CSV file to start practicing ")
-	lookCSV = flag.String("look", "", "Path for CSV file and see how the cards are organized")
+	learnCSV = flag.String("learn", "", "Path for CSV file to learn your cards ")
+	playCSV  = flag.String("play", "", "Path for CSV file to start practicing ")
+	lookCSV  = flag.String("look", "", "Path for CSV file and see how the cards are organized ")
 )
 
 func main() {
 
 	flag.Parse()
 
-	if *playCSV != "" {
+	if *learnCSV != ""{
+
+		openedCSV := cards.ReadCSV(*learnCSV)
+		cards.Learn(openedCSV)
+
+	} else if *playCSV != "" {
 
 		openedCSV := cards.ReadCSV(*playCSV)
 		cards.Practice(openedCSV)
@@ -24,9 +30,10 @@ func main() {
 	} else if *lookCSV != "" {
 
 		for i := range cards.ReadCSV(*lookCSV) {
-			fmt.Println(cards.ReadCSV(*lookCSV)[i])
-		}
 
+			fmt.Println(cards.ReadCSV(*lookCSV)[i])
+
+		}
 
 	} else {
 		flag.Usage()
