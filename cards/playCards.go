785e5f1e139f5d40573174f5179d5a8a19bb873d
rafa-leao <rleao53@gmail.com>
2019-06-19 14:30:11 -0300

    New file to handle with card-wise

--- cards/playCards.go
+++ cards/playCards.go
@@ -7,66 +7,32 @@ import (
 	"time"
 )
 
-type deckFace struct {
-	front string
-	back  string
-}
-
-func CreateSliceOfCards(csvFile [][]string) (cardsToPractice []deckFace) {
-
-	cardsToPractice = make([]deckFace, len(csvFile))
-
-	for i, card := range csvFile {
-
-		cardsToPractice[i] = deckFace{
-			front: card[0],
-			back:  card[1],
-		}
-	}
-
-	return
-}
-
 func randomNumber(number int) int {
 
 	return rand.New(rand.NewSource(time.Now().UnixNano())).Intn(number)
 
 }
 
-func cardInCardsAnswered(card deckFace, cardsAnswered []deckFace) bool {
-
-	for _, cardAnswered := range cardsAnswered {
-
-		if card == cardAnswered {
-			return true
-		}
-	}
-
-	return false
-
-}
-
 func Practice(csvFile [][]string) {
 
-	cardsToPractice := CreateSliceOfCards(csvFile)
-
 	var (
-		card            deckFace
-		cardsAnswered []deckFace
+		cardsToPractice []Card = CreateSliceOfCards(csvFile)
+		cardsAnswered   []Card
+		card              Card
 	)
 
 	for totalOfCards := len(cardsToPractice); totalOfCards > len(cardsAnswered); {
 
 		card = cardsToPractice[randomNumber(len(cardsToPractice))]
 
-		if !cardInCardsAnswered(card, cardsAnswered) {
+		if !IsCardInSliceOfCards(card, cardsAnswered) {
 
-			fmt.Printf("%s = ", card.front)
+			fmt.Printf("%s = ", card.frontFace)
 
 			var answer string
 			fmt.Scan(&answer)
 
-			if answer != card.back {
+			if answer != card.backFace {
 				fmt.Println("Wrong answer. Do not give up! Try again!")
 				os.Exit(1)
 			} else {
